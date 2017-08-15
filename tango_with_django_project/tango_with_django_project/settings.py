54397e9962bd6daf8de9d322db2d77466d5762aa
valentineisme <valentineisme@gmail.com>
2017-08-15 11:28:08 -0300

    static images

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -120,4 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    STATIC_DIR,
+)
 STATIC_URL = '/static/'
