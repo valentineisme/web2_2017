1b2c3e36c2bc6f095e47ebac227211e0565472da
valentineisme <valentineisme@gmail.com>
2017-09-05 11:12:00 -0300

    modesl-view w likes

--- tango_with_django_project/populate.py
+++ tango_with_django_project/populate.py
@@ -1,68 +1,53 @@
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+#!/usr/bin/env python3
 
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'tango_with_django_project.settings')
 import django
 django.setup()
-
 from rango.models import Category, Page
 
-
 def populate():
-    python_cat = add_cat('Python')
+    categories = open('populate_rango_categories.csv').readlines()
 
-    add_page(cat=python_cat,
-        title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+    print('Populating %d categories...' %len(categories))
+    for categorie in categories:
+        cat, views, likes = categorie.strip().split(',')
+        c = add_cat(cat, views, likes)
 
-    add_page(cat=python_cat,
-        title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+    pages = open('populate_rango_pages.csv').readlines()
+    print('Populating %d pages...' %len(pages))
+    for page in pages:
+        cat, title, url = page.strip().split(',')
+        c = add_cat(cat)
+        ok = add_page(c, title, url)
 
-    add_page(cat=python_cat,
-        title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
-
-    django_cat = add_cat("Django")
-
-    add_page(cat=django_cat,
-        title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
-
-    add_page(cat=django_cat,
-        title="Django Rocks",
-        url="http://www.djangorocks.com/")
-
-    add_page(cat=django_cat,
-        title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
-
-    frame_cat = add_cat("Other Frameworks")
-
-    add_page(cat=frame_cat,
-        title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
-
-    add_page(cat=frame_cat,
-        title="Flask",
-        url="http://flask.pocoo.org")
-
-    # Print out what we have added to the user.
+def show():
+    # Print out the categories we have added.
+    print('Showing pages...')
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
-            print "- {0} - {1}".format(str(c), str(p))
+            print("- {0} - {1}".format(str(c), str(p)))
+
 
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
+    p.url = url
+    p.views = views
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    if not c.views:
+        c.views = views
+    if not c.likes:
+        c.likes = likes
+    c.save()
     return c
 
 # Start execution here!
 if __name__ == '__main__':
-    print "Starting Rango population script..."
+    print("Starting Rango population script...")
     populate()
+    show()
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,10 +1,17 @@
 from __future__ import unicode_literals
+
 from django.db import models
 
+# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
-    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
         return self.name
 
 class Page(models.Model):
@@ -13,6 +20,5 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    def __unicode__(self):      #For Python 2, use __str__ on Python 3
+    def __str__(self):
         return self.title
-# Create your models here.
