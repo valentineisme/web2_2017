e579f25f4b6aaa03c00549e6301b1465f9872f28
valentineisme <valentineisme@gmail.com>
2017-09-05 10:44:44 -0300

    models

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-	context_dict = {'boldmessage':'eu quero morrer'}
+	context_dict = {'boldmessage':'oi'}
 	return render(request, 'rango/index.html', context_dict)
 	
 def about(request):
-	return HttpResponse("ainda quero morrer <br><br> <a href='/rango/'> inicial </a>")
+	return HttpResponse("sobre <br><br> <a href='/rango/'> inicial </a>")
 # Create your views here.
